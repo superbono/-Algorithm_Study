029c8d6117862f3da4593bfee82fb3016f805f5e
superbono3300 <cbman87@naver.com>
2020-12-24 14:08:53 +0900

    question 7  -> commit

---
+++ CodeUp/src/q_7/Main.java
@@ -0,0 +1,12 @@
+package q_7;
+
+public class Main {
+
+	//"C:\Download\hello.cpp"
+	//(단, 큰따옴표도 함께 출력한다.)
+	
+	public static void main(String[] args) {
+		System.out.println("\"C:\\Download\\hello.cpp\"");
+	}
+
+}
