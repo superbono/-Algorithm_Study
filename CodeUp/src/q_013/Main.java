14095f9541f7135759f6a6d13cbcc68c02b2d4a2
superbono <hiperbono@naver.com>
2020-12-26 00:09:02 +0900

    question 13  -> commit

---
+++ CodeUp/src/q_013/Main.java
@@ -0,0 +1,16 @@
+package q_013;
+
+import java.util.Scanner;
+
+public class Main {
+
+	
+	// 정수(int) 2개를 입력받아 그대로 출력해보자.
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num1 = sc.nextInt();
+		int num2 = sc.nextInt();
+		System.out.printf("%d %d",num1,num2);
+	}
+
+}
