cdd58769a6ef5fe8c2382e63aa52afb922959fd9
superbono3300 <cbman87@naver.com>
2020-12-25 15:25:23 +0900

    question 10  -> commit

---
+++ CodeUp/src/q_010/Main.java
@@ -0,0 +1,15 @@
+package q_010;
+
+import java.util.Scanner;
+
+public class Main {
+
+	// 정수형(int)으로 변수를 선언하고, 변수에 정수값을 저장한 후
+	// 변수에 저장되어 있는 값을 그대로 출력해보자.
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		System.out.println(num);
+	}
+
+}
