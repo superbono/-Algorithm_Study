62697fe56c0b6ad9ab1260ca7e0412e6fb463e29
superbono <hiperbono@naver.com>
2020-12-26 00:36:25 +0900

    question 18  -> commit

---
+++ CodeUp/src/q_018/Main.java
@@ -0,0 +1,14 @@
+package q_018;
+
+import java.util.Scanner;
+
+public class Main {
+
+	// 어떤 형식에 맞추어 시간이 입력될 때, 그대로 출력하는 연습을 해보자.
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String time = sc.nextLine();
+		System.out.printf("%s",time);
+	}
+
+}
