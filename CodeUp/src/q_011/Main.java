d8c79e41761a1c3fe5cfafc461faa85ca0b23bc9
superbono <hiperbono@naver.com>
2020-12-25 15:31:09 +0900

    question 11  -> commit

---
+++ CodeUp/src/q_011/Main.java
@@ -0,0 +1,15 @@
+package q_011;
+
+import java.util.Scanner;
+
+public class Main {
+
+	// 문자형(char)으로 변수를 하나 선언하고, 변수에 문자를 저장한 후
+	// 변수에 저장되어 있는 문자를 그대로 출력해보자.
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine();
+		System.out.println(str);
+	}
+
+}
