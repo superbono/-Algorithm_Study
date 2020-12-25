96211e2d880c2edf3914e98234df4da6c14c7393
superbono <hiperbono@naver.com>
2020-12-25 15:35:36 +0900

    question 12  -> commit

---
+++ CodeUp/src/q_012/Main.java
@@ -0,0 +1,15 @@
+package q_012;
+
+import java.util.Scanner;
+
+public class Main {
+
+	// 실수형(float)로 변수를 선언하고 그 변수에 실수값을 저장한 후
+	// 저장되어 있는 실수값을 출력해보자.
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		float fl = sc.nextFloat();
+		System.out.printf("%f",fl);
+	}
+
+}
