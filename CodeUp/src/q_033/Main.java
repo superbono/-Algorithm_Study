1160fd2107125652d41761d07b7daee9d7c9df6b
superbono <hiperbono@naver.com>
2020-12-26 20:23:33 +0900

    question 33  -> commit

---
+++ CodeUp/src/q_033/Main.java
@@ -0,0 +1,14 @@
+package q_033;
+
+import java.util.Scanner;
+
+public class Main {
+
+	// 10진수를 입력받아 16진수(hexadecimal)로 출력해보자.
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		System.out.printf("%X",num);
+	}
+
+}
