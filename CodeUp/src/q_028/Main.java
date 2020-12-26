d1c3dd632ad0d1970ed49cedffcfb2bd995c4342
superbono <hiperbono@naver.com>
2020-12-26 19:36:53 +0900

    question 28  -> commit

---
+++ CodeUp/src/q_028/Main.java
@@ -0,0 +1,15 @@
+package q_028;
+
+import java.util.Scanner;
+
+public class Main {
+
+	// 정수 1개를 입력받아 그대로 출력해보자.
+	// (단, 입력되는 정수의 범위는 0 ~ 4,294,967,295 이다.)
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		long num = sc.nextLong();
+		System.out.println(num);
+	}
+
+}
