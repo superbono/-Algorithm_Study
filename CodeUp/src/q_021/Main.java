e432f35f786d7ae23b4215cb16a07edfdc3b2654
superbono <hiperbono@naver.com>
2020-12-26 12:58:00 +0900

    question 21  -> commit

---
+++ CodeUp/src/q_021/Main.java
@@ -0,0 +1,14 @@
+package q_021;
+
+import java.util.Scanner;
+
+public class Main {
+
+	// 1개의 단어를 입력받아 그대로 출력해보자.
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine();
+		System.out.println(str);
+	}
+
+}
