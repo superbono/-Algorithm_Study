7a83a3baad79442e2c744ffed6714ffa8a0169b5
superbono <hiperbono@naver.com>
2020-12-26 15:32:43 +0900

    question 24  -> commit

---
+++ CodeUp/src/q_24/Main.java
@@ -0,0 +1,21 @@
+package q_24;
+
+import java.util.Scanner;
+
+public class Main {
+
+	// 단어를 1개 입력받는다.
+	// 입력받은 단어(영어)의 각 문자를
+	// 한줄에 한 문자씩 분리해 출력한다.
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String word  = sc.next();
+		
+		for (int i=0;i<word.length();i++) {
+			System.out.printf("'%s'\n",word.charAt(i));
+		}
+		
+		
+	}
+
+}
