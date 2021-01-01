2b56df587c9e50713a3b6aed0d858712045bf692
superbono <hiperbono@naver.com>
2021-01-01 21:55:46 +0900

    question 95  -> commit

--- CodeUp/src/q_095/Main.java
+++ CodeUp/src/q_095/Main.java
@@ -1,4 +1,4 @@
-package q_094;
+package q_095;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -8,31 +8,33 @@ public class Main {
 
 	// 번호를 부른 횟수(n, 1 ~ 10000)가 첫 줄에 입력된다.
 	// n개의 랜덤 번호(k, 1 ~ 23)가 두 번째 줄에 공백을 사이에 두고 순서대로 입력된다.
-	// 출석을 부른 번호 순서를 바꾸어 공백을 두고 출력한다.
+	// 출석을 부른 번호 중에 가장 빠른 번호를 1개만 출력한다.
 	
 	// 10
 	// 10 4 2 3 6 6 7 9 8 5
 	
-	// 5 8 9 7 6 6 3 2 4 10
+	// 2 출력 
 	
 	public static void main(String[] args) {
+		
 		Scanner sc = new Scanner(System.in);
 		
-		int i,n,t;
-		n = sc.nextInt();
+		int i,n,num;
 		ArrayList<Integer> a = new ArrayList<>();
+		n = sc.nextInt();
 		
 		for(i=0;i<n;i++) {
-			t = sc.nextInt();
-			a.add(t);
+			num = sc.nextInt();
+			a.add(num);
 		}
 		
-		Collections.reverse(a);
+		Collections.sort(a);
 		
-		for(i=0;i<a.size();i++) {
-			System.out.printf("%d ",a.get(i));
+		for(i=0;i<n;i++) {
+			
 		}
 		
+		System.out.printf("%d ",a.get(0));
 		
 	}
 
